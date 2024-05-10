0a6c52024956eeb2fe50a657d7f866182c841bb8
hyodong <hyodong123@gmail.com>
2024-05-10 15:08:17 +0900

    hello

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -2,7 +2,7 @@ package test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world!!!!!!!!!!!!!!!!!!");
+		System.out.println("Hello ㅋㅋ world!!!!!!!!!!!!!!!!!!");
 	}
 
 
